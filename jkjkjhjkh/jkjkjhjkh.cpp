d4975212b03e1951703e26a62a1171fd3971769a
Agagag722 <157271433+Yanesnal@users.noreply.github.com>
2025-11-20 13:25:04 +0300

    Add files via upload

---
+++ jkjkjhjkh/jkjkjhjkh.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+int main()
+{
+	double a, b, h, x, y, s, p;
+	int n, i;
+	cout << "Vvedite a,b,n" << endl;
+	cin >> a >> b >> n;
+	h = (b - a) / 10;
+	x = a;
+	do
+	{
+		p = s = x;
+		for (i = 1; i <= n; i++)
+		{
+			p *= pow(x, 2) * pow(-1, i) / (2 * i + 1);
+			s += p;
+		}
+		y = atan(x);
+		cout << setw(15) << x << setw(15) << y << setw(15) << s << endl;
+		x += h;
+	} while (x <= b + h / 2);
+	cout << endl;
+	return 0;
+}
